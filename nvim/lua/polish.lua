4bb8498eb7c1eeacfbbba62ca6f0c6e47eb73ce7
Vadims <fvdev9@gmail.com>
2025-04-19 22:34:08 +0200

    upgrade to v5

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -1,29 +1,5 @@
-vim.filetype.add {
-  extension = {
-    foo = "fooscript",
-  },
-  filename = {
-    ["Foofile"] = "fooscript",
-  },
-  pattern = {
-    ["~/%.config/foo/.*"] = "fooscript",
-  },
-}
+if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "php",
-  callback = function()
-    vim.opt_local.tabstop = 4
-    vim.opt_local.shiftwidth = 4
-    vim.opt_local.expandtab = true
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  pattern = "*.php",
-  callback = function()
-    vim.opt_local.tabstop = 4
-    vim.opt_local.shiftwidth = 4
-    vim.opt_local.expandtab = true
-  end,
-})
+-- This will run last in the setup process.
+-- This is just pure lua so anything that doesn't
+-- fit in the normal config locations above can go here
