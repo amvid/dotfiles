5c6e992123dc2a51e1c76a469d44c73d3c1fc0b9
Vadims <fvdev9@gmail.com>
2025-12-06 23:53:28 +0100

    upd

---
+++ nvim/lua/plugins/lazy-docker.lua
@@ -0,0 +1,23 @@
+---@type LazySpec
+return {
+  {
+    "mgierada/lazydocker.nvim",
+    lazy = true,
+    dependencies = {
+      "akinsho/toggleterm.nvim",
+      {
+        "AstroNvim/astrocore",
+        opts = function(_, opts)
+          local maps = opts.mappings
+
+          local prefix = "<Leader>g"
+
+          maps.n[prefix .. "d"] = {
+            function() require("lazydocker").open() end,
+            desc = "ToggleTerm LazyDocker",
+          }
+        end,
+      },
+    },
+  },
+}
