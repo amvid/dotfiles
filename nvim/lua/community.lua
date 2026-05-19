1b0c17401be4e2cce437f1d9c383ea552d4b01df
Vadims <fvdev9@gmail.com>
2026-05-19 23:33:50 +0200

    nvim

---
+++ nvim/lua/community.lua
@@ -0,0 +1,28 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.biome" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.nginx" },
+  { import = "astrocommunity.pack.prettier" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.sql" },
+
+}
